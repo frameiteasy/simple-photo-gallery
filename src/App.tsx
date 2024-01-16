fb26c20440430609ee5b3e7aa0ee2482e26fb24f
Konrad Marciniak <konrad@Konrads-MacBook-Air.local>
2024-01-16 20:32:15 +0000

    basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="app_container">
+      <header id="app_header">
+        <div id="app_header_title">Photo Gallery</div>
+        <div id="app_header_subtitle">Konrad Marciniak</div>
       </header>
+      <div id="app_content">ala ma kota</div>
+      <div id="app_footer">footer</div>
     </div>
   );
 }
